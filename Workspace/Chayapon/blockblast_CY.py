3033703a02c76939cb54bfc49953e384e70d4851
Chayapon Yaram <pongisnotgood3@gmail.com>
2026-09-15 14:10:34 +0700

    fourth step

--- Workspace/Chayapon/blockblast_CY.py
+++ Workspace/Chayapon/blockblast_CY.py
@@ -2,3 +2,26 @@ from processing import *
 import random
 
 class Board:
+    def __init__():
+class Piece:
+    def __init__():
+
+def setup():
+    size()
+    frameRate(60)
+
+def draw():
+
+def can_place():
+
+def place():
+
+def clear_lines():
+
+def mousePressed():
+
+def mouseDragged():
+
+def mouseReleased():
+
+run()
