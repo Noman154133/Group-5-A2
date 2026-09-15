3899f72492ff4f7744ab4323748c1012f00f37f2
Chayapon Yaram <pongisnotgood3@gmail.com>
2026-09-15 14:18:08 +0700

    Test fourth step

--- Workspace/Chayapon/blockblast_CY.py
+++ Workspace/Chayapon/blockblast_CY.py
@@ -1,8 +1,15 @@
 from processing import *
 import random
 
+PALETTE =  [(245, 93, 62),   # Orange-Red
+            (66, 133, 244),  # Blue
+            (52, 168, 83),   # Green
+            (251, 188, 5),   # Yellow
+            (171, 71, 188)]  # Purple
+
 class Board:
     def __init__():
+        
 class Piece:
     def __init__():
 
